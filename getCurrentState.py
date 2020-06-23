34a8de6d1a1df383dd3078bfb2693d481a973fce
nhnthanh <64197535+nhnthanh194@users.noreply.github.com>
2020-06-23 14:04:44 +0700

    Update Disk function
    Added more disk function
    Edited print layout

--- getCurrentState.py
+++ getCurrentState.py
@@ -1,17 +1,22 @@
 import os,parser,sys,sysconfig,subprocess
 import socketserver,socket
 # PS Ultility
+# https://www.thepythoncode.com/article/get-hardware-system-information-python#Disk_Usage
 import psutil
+import platform
 
 print("_"*40)
+print("Your System Information")
+print("_"*40)
 """ Get current system like: 
-Standard Fearture: SELINUX, IP, Hostname, Firewall, Disks, FSTab
+Standard Fearture: SELINUX, IP, Hostname, Firewall, Disks, mount point
 """
 dict_state={}
 # Function get current SE-Linux Setting
 def selinux_State():
-    sestatus=subprocess.run('sestatus',capture_output=True,check=True)
-    if str(sestatus).find('disabled'):
+    sestatus=subprocess.run('sestatus',stderr=subprocess.PIPE,stdout=subprocess.PIPE,check=True)
+    sestatus_decode=sestatus.stdout.decode()
+    if str(sestatus_decode.find('disabled')):
         return 'Disabled'
     return 'Enabled'
 # Funtion get IP 
@@ -25,27 +30,36 @@ def get_OSrelease():
 def get_FW():
     #[2] is release version 
     if get_OSrelease()[2].find('amz'):
-        return 'Your OS has no Firewall'
+        return f'Your OS has no Firewall ({get_OSrelease()[2]})'
     else:
         return 'Disabled'
 # Funtion get Disks Drive
-def get_disks():
-    disk=psutil.get_disks()
-    return
+def get_Disks():
+    # Get Disk
+    disk_list=[]
+    disk_mountpoint=[]
+    disk=psutil.disk_partitions()
+    for item in disk:
+        disk_list.append(item.device)
+        disk_mountpoint.append(item.mountpoint)
+    return disk_list,disk_mountpoint
 
 # Khởi tạo tuple IPHostname = function get_IPName, vì nó return 2 giá trị
 IPHostname=get_IPName()
 dict_state['Selinux']=selinux_State()
-dict_state['IP']=IPHostname[0]
+dict_state['Your IP']=IPHostname[0]
 dict_state['Hostname']=IPHostname[1]
 dict_state['Firewall']=get_FW()
+dict_state['Disk List']=get_Disks()[0]
+dict_state['Mount point']=get_Disks()[1]
 
-# print(dict_state)
-# for key,value in dict_state.items():
-#     print(f"[{key}]:{value}")
+for key,value in dict_state.items():
+    #print(f"[{key}]:{value}")
+    print("%s \t\t %s"%(key,value))
 
-for row in dict_state.items():
-    print("|{:<20}|{:<20}".format(*row),sep="|")
+#for row in dict_state.items():
+#    print("{:<20}| {:<20}".format(*row),sep="|")
 
 print("_"*40)
-print(psutil.get_disks())
+
+
